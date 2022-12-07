5faa2f32980efa2ae5428d9b529d74892a611eee
ifnu <ifnuu01@gmail.com>
2022-12-07 16:29:50 +0800

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
+import numpy as np
 import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
-import numpy as np
 
-
-ops=st.sidebar.radio("MENU", options=("HOME", "PROGRAM", "ABOUT"))
+ops=st.sidebar.radio("MENU", options=("HOME", "PROGRAM", "ABOUT", "CONTACT"))
 
 if ops=="HOME":
-    st.title("DETEKSI KEMATANGAN BUAH TOMAT")
-    st.write("Mengklasifikasi gambar tomat dengan kategori matang, setengah matang, dan mentah.")
+    st.title("DETEKSI KEMATANGAN SAYUR TOMAT")
+    st.write("Mengklasifikasi gambar (SAYUR) dengan kategori matang, setengah matang, dan mentah.")
 
 
     st.caption('Contoh gambar tomat:')
@@ -20,12 +19,12 @@ if ops=="HOME":
         st.header('TOMAT SETENGAH MATANG')
         st.image('SETENGAH MATANG.jpg')
     with col3:
-        st.header('TOMAT MENTAh')
+        st.header('TOMAT MENTAH')
         st.image('MENTAH.jpg') 
 
 
 elif ops=="PROGRAM":
-    st.title('PROGRAM DETEKSI KEMATANGAN BUAH TOMAT')
+    st.title('PROGRAM DETEKSI KEMATANGAN SAYUR TOMAT')
     st.image('laptop-3706810__340.webp',caption=('Click Browse Files'))
     
     file = st.file_uploader("Pastikan Gambar Bertype JPG/JPEG", type=['jpg','jpeg'])
@@ -60,18 +59,80 @@ elif ops=="PROGRAM":
             st.text("Probalitas (0: Tomat Matang, 1: Tomat Mentah, 2: Tomat Setengah Matang)")
             st.write(prediction)
             
-else:
-    st.title('KELOMPOK 3')
+if ops=="ABOUT": 
+    st.title('KELOMPOK 1')
     st.caption('SMK N 5 Samarinda')
 
-    col1, col2 =st.columns(2)
+    col1, =st.columns(1)
+    col2, col3, =st.columns(2)
+    col4, col5, =st.columns(2)
 
     with col1:
-        st.image('photo_2022-04-11_08-19-49.jpg')
+        st.image('Grub1.jpeg')
         st.write('Ifnu Umar')
-        st.caption('Program & Web')
+        st.caption('Ketua (Modeling, Program, Dataset)')
     
     with col2:
-        st.image('photo_2022-07-09_21-04-25.jpg')
-        st.write('Maruto Nugroho')
-        st.caption('Dataset')
+        st.image('Grub2.jpeg')
+        st.write('Muhammad Arif Rahman')
+        st.caption('Web Design')
+    
+    with col3:
+        st.image('Grub3.jpeg')
+        st.write('Akmal')
+        st.caption('........')
+    
+    with col4:
+        st.image('Grub4.jpeg')
+        st.write('Rio febrian')
+        st.caption('........')
+   
+    with col5:
+        st.image('Grub5.jpeg')
+        st.write('Rafael')
+        st.caption('........')
+    
+    st.write("Kami membuat website ini untuk mendescripsikan tomat itu dengan kategori Matang/Setengah Matang/Mentah dengan Mengklasifikasi gambar (BUAH).")
+
+elif ops=="CONTACT":
+    st.title('Sosial Media')
+
+    col1, col2, col3, col4, col5 = st.columns( 5)
+    col1.write("WhatsApp")
+    col2.write("Instagram")
+    col3.write("Facebook")
+    col4.write("Youtube")
+    col5.write("Twitter")
+        
+    with col1:
+        st.image('whatsapp.webp')
+
+        if st.write("[Whatsapp](https://api.whatsapp.com/send/?phone=6289501603099&text=Bang+Save+Saya+%28Nama%29)"):
+            st.write()
+   
+    with col2:
+        st.image('Instagram.png')
+
+        if st.write("[Instagram](https://www.instagram.com)"):
+            st.write()
+   
+    with col3:
+        st.image('Facebook.png')
+
+        if st.write("[Facebook](https://www.facebook.com)"):
+            st.write()
+    
+    with col4:
+        st.image('Youtube.jpg')
+
+        if st.write("[Youtube](https://www.youtube.com)"):
+            st.write()
+
+    with col5:
+        st.image('Twitter.png')
+
+        if st.write("[Twitter](https://www.twitter.com)"):
+            st.write()
+
+        
+            
